74ce59abf62ff273ff621a238b20101f1baac693
NareshGuru77 <gurulingan.nareshkumar@gmail.com>
2019-02-20 15:46:19 +0100

    able to get circle rois

--- logo_detector.py
+++ logo_detector.py
@@ -1,39 +1,66 @@
 import cv2
 import numpy as np
-from skimage import measure, draw
 
+
+def get_circle_regions(image):
+    image = image.copy()
+    img_blur = cv2.medianBlur(image, 3)
+    img_gray = cv2.cvtColor(img_blur, cv2.COLOR_BGR2GRAY)
+    thresholded = cv2.adaptiveThreshold(
+        img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY, 11, 2)
+
+    el = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    eroded = cv2.erode(thresholded, el, iterations=1)
+    circles = cv2.HoughCircles(eroded, cv2.HOUGH_GRADIENT,
+                               1, img.shape[0] / 8, param1=300,
+                               param2=30, minRadius=1,
+                               maxRadius=100)
+
+    roi = []
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            center = (i[0], i[1])
+            radius = i[2]
+            cv2.circle(image, center, radius,
+                       (255, 0, 255), 3)
+            x, y = i[0] - i[2], i[1] - i[2]
+            side = 2 * i[2]
+            roi.append([x, y, side])
+
+    return roi, image
+
+
+def get_h_regions(image):
+    image = image.copy()
+    img_blur = cv2.medianBlur(image, 3)
+    img_gray = cv2.cvtColor(img_blur, cv2.COLOR_BGR2GRAY)
+    thresholded = cv2.adaptiveThreshold(
+        img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY, 11, 2)
+    kernel = np.ones((3, 3), np.uint8)
+    # eroded = cv2.erode(thresholded, kernel, iterations=2)
+    dilated = cv2.dilate(thresholded, kernel, iterations=2)
+    edged = cv2.Canny(thresholded, 30, 200)
+    contours = cv2.findContours(thresholded.copy(), cv2.RETR_LIST,
+                            cv2.CHAIN_APPROX_SIMPLE)[0]
+    for c in contours:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.1 * peri, True)
+        if len(approx) == 4:
+            screenCnt = approx
+            cv2.drawContours(image, c, -1, (0, 255, 0), 3)
+    cv2.imshow('thresholded', thresholded)
+    cv2.imshow('eroded', dilated)
+    cv2.imshow('edges', edged)
+    cv2.imshow('image', image)
+    cv2.waitKey(0)
+
+    roi = []
+
+    return roi, image
 
 img = cv2.imread('./images/bosch-logo.jpg')
-org_img = img.copy()
-kernel = np.ones((5,5),np.uint8)
-erosion = cv2.erode(img,kernel,iterations = 2)
-img_blur = cv2.medianBlur(erosion, 5)
-img_gray = cv2.cvtColor(img_blur, cv2.COLOR_BGR2GRAY)
-
-cv2.imshow('erode', erosion)
-th3 = cv2.adaptiveThreshold(img_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv2.THRESH_BINARY,11,2)
-edge_image = cv2.Canny(th3, 175, 200)
-cv2.imshow('edge_image', edge_image)
-
-coords = np.column_stack(np.nonzero(edge_image))
-model, inliers = measure.ransac(coords, measure.CircleModel,
-                                min_samples=3, residual_threshold=1,
-                                max_trials=1000)
-
-rr, cc = draw.circle_perimeter(int(model.params[0]),
-                               int(model.params[1]),
-                               int(model.params[2]),
-                               shape=img.shape)
-
-zero_img = np.zeros(img.shape[:2], dtype=np.uint8)
-zero_img[rr, cc] = 255
-
-contours,hierarchy = cv2.findContours(zero_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-cnt = contours[0]
-x, y, w, h = cv2.boundingRect(cnt)
-cv2.rectangle(org_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-cv2.imshow('edge', zero_img)
-cv2.imshow('result', org_img)
-cv2.waitKey(0)
+circle_roi, _ = get_circle_regions(img)
+h_roi, _ = get_h_regions(img)
